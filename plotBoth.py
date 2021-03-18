83e96b4dd32f80c269971f611801175b52b61b04
Laima <laima.busaite@gmail.com>
2021-03-18 18:00:18 +0200

    files

--- plotBoth.py
+++ plotBoth.py
@@ -5,12 +5,19 @@ from plotTheory import plotRabiDependence
 
 if __name__ == "__main__":
 
-    rabiList = [0.5, 1, 5, 10, 20, 50, 100]
-    transitionList = ['3-3', '3-4', '4-4', '4-3']
-    probeList = ['3-3', '3-3', '4-4', '4-4']
+    rabiList = [0.5, 1, 5]#, 10, 20, 50, 100]
+    # transitionList = ['3-2', '3-3', '3-4', '4-3', '4-4', '4-5']
+    # probeList = transitionList #['3-3', '3-4', '4-4', '4-3']
+    # # plotBx(rabiList, transitionList,probeList)
+    # # plotBy(rabiList, transitionList, probeList)
+    # plotRabiDependence('Bx', rabiList, transitionList, probeList, dline=2, x_range = 10)
+    # plotRabiDependence('By', rabiList, transitionList, probeList, dline=2)
+
+    transitionList = ['3-3', '3-4', '4-3', '4-4']
+    probeList = transitionList  # ['3-3', '3-4', '4-4', '4-3']
     # plotBx(rabiList, transitionList,probeList)
     # plotBy(rabiList, transitionList, probeList)
-    plotRabiDependence('Bx', rabiList, transitionList, probeList)
-    plotRabiDependence('By', rabiList, transitionList, probeList)
+    plotRabiDependence('Bx', rabiList, transitionList, probeList, dline=1, x_range = 10)
+    # plotRabiDependence('By', rabiList, transitionList, probeList, dline=2)
 
     plt.show()
--- plotTheory.py
+++ plotTheory.py
@@ -6,7 +6,7 @@ import pandas as pd
 # theorPath = 'C:\\Users\\laima\\OneDrive - University of Latvia\\Pētījumi\\Atomi\\Cs magnetometrs\Calculations-v1\\D1pumpBx\\Pump_D1-Cs133_Probe_D1-Cs133\gamma=0.0190-DSteps=150-DScan=2.00sigma-lWidth=2.0\\Pump_3-3-theta=1.57-phi=1.57-pol=0-det=0_Probe_3-3-det=0\\Absorption_theta=1.57-0.79_phi=0.79-1.57_pol=0-0_gammaprobe=0.0190_lWidthpr=2.0\\Absorption_Cs133-D1-PumpRabi=0.50-shift=0.0-Ge=0'
 
 
-def plotRabiDependence(Bdirection, rabiList, transitionList, probeList=None):
+def plotRabiDependence(Bdirection, rabiList, transitionList, probeList=None, dline=1, x_range = None):
     rabiList = np.array(rabiList)
     transitionList = np.array(transitionList)
     if probeList == None:
@@ -14,7 +14,7 @@ def plotRabiDependence(Bdirection, rabiList, transitionList, probeList=None):
 
     fig = plt.figure()
     fig.canvas.set_window_title(Bdirection)
-    plt.suptitle(Bdirection)
+    plt.suptitle(f'D{dline}-{Bdirection}')
     nrows = 2
     ncols = len(transitionList)//2
 
@@ -24,20 +24,25 @@ def plotRabiDependence(Bdirection, rabiList, transitionList, probeList=None):
         for rabi in rabiList:
 
             # theorPath = '../D1pumpBx/Pump_D1-Cs133_Probe_D1-Cs133/Absorption_Cs133-D1-PumpRabi=0.50-shift=0.0-Ge=0'
-            theorPath = f'./D1pump{Bdirection}/Pump_D1-Cs133_Probe_D1-Cs133/{transition}/Absorption_Cs133-D1-PumpRabi={rabi:.2f}-shift=0.0-Ge=0'
+            theorPath = f'./D{dline}pump{Bdirection}/Pump_D{dline}-Cs133_Probe_D{dline}-Cs133/{transition}/Absorption_Cs133-D{dline}-PumpRabi={rabi:.2f}-shift=0.0-Ge=0'
 
 
             theorData = pd.read_csv(theorPath, header=None, delimiter=' ', names=['B', 'Total', 'comp1', 'comp2', 'diff'])
 
             print(theorData)
 
-            plt.plot(theorData['B'], theorData['comp1'], label=rabi)
+            max_x_range = 1e20
+            if x_range:
+                max_x_range = x_range
+            condition = np.abs(theorData['B'])<=max_x_range
+
+            plt.plot(theorData['B'][condition], theorData['comp1'][condition], label=rabi)
             plt.legend()
 
 if __name__ == '__main__':
-    rabiList = [0.5, 1, 5, 10, 20, 50, 100]
+    rabiList = [0.5, 1, 5]#, 10, 20, 50, 100]
     transitionList = ['3-3', '3-4', '4-4', '4-3']
-    probeList = ['3-3', '3-3', '4-4', '4-4']
+    probeList = ['3-3', '3-4', '4-4', '4-3']
     plotRabiDependence('Bx', rabiList, transitionList, probeList)
 
     plt.show()
